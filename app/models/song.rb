d82651ac30414764612050215d551b8ea67f9fc2
Nolan Byrnes <nolanehs@gmail.com>
2018-01-11 09:44:33 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,8 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
+drake = self.artist(:name = "Drake")
+drake
     # when this method is called it should assign the song's artist to Drake
   end
 end
